0a49d8e5febe7233c34dc70fc1ceaa06abf9495e
Cris Hall-Ramos <crishallramos@yahoo.com>
2016-07-20 17:10:15 -0400

    Add Request Flags

--- TwitchCom/Chat.cs
+++ TwitchCom/Chat.cs
@@ -14,31 +14,56 @@ namespace TwitchCom
         private StreamReader inputStream;
         private StreamWriter outputStream;
         private Twitch twitch;
-        public bool Connected { get; set; }
+        private bool connected = false;
+        public bool Connected { get { return connected; } }
+        public bool RequestTags { get; set; } = false;
+        public bool RequestCommands { get; set; } = false;
+        public bool RequestMembership { get; set; } = false;
+        public bool RequestAll { get; set; } = false;
 
         public Chat(Twitch _t)
         {
             twitch = _t;
             tcpClient = new TcpClient();
+            Connect();
+        }
+
+        public void Connect()
+        {
             tcpClient.Connect("irc.chat.twitch.tv", 6667);
 
             if (tcpClient.Connected)
-                Connected = true;
+                connected = true;
             else
             {
-                Connected = false;
+                connected = false;
                 return;
             }
 
             inputStream = new StreamReader(tcpClient.GetStream());
             outputStream = new StreamWriter(tcpClient.GetStream());
 
-            outputStream.WriteLine("PASS " + _t.OAuth);
-            outputStream.WriteLine("NICK " + _t.UserName);
-            outputStream.WriteLine("USER " + _t.UserName + " 8 * :" + _t.UserName);
-            outputStream.WriteLine("CAP REQ :twitch.tv/membership");
-            outputStream.WriteLine("CAP REQ :twitch.tv/commands");
-            outputStream.WriteLine("CAP REQ :twitch.tv/tags");
+            outputStream.WriteLine("PASS " + twitch.OAuth);
+            outputStream.WriteLine("NICK " + twitch.UserName);
+            outputStream.WriteLine("USER " + twitch.UserName + " 8 * :" + twitch.UserName);
+
+            if (RequestMembership || RequestAll)
+                outputStream.WriteLine("CAP REQ :twitch.tv/membership");
+            if (RequestCommands || RequestAll)
+                outputStream.WriteLine("CAP REQ :twitch.tv/commands");
+            if (RequestTags || RequestAll)
+                outputStream.WriteLine("CAP REQ :twitch.tv/tags");
+        }
+
+        public void ReConnect()
+        {
+            if(Connected)
+            {
+                tcpClient.Close();
+                connected = false;
+            }
+
+            Connect();
         }
 
         public Message readMessage()
@@ -85,65 +110,74 @@ namespace TwitchCom
                 }
             }
 
-            switch (type)
+            if (RequestTags || RequestAll)
             {
-                case Messages.Type.PRIVMSG:
-                    PRVMSG pv_ret = new PRVMSG();
-                    pv_ret.ParseTags(split_raw[0]);
-                    output = pv_ret;
-                    break;
-                case Messages.Type.NOTICE:
-                    NOTICE note_ret = new NOTICE();
-                    note_ret.ParseTags(split_raw[0]);
-                    output = note_ret;
-                    break;
+                switch (type)
+                {
+                    case Messages.Type.PRIVMSG:
+                        PRVMSG pv_ret = new PRVMSG();
+                        pv_ret.ParseTags(split_raw[0]);
+                        output = pv_ret;
+                        break;
+                    case Messages.Type.NOTICE:
+                        NOTICE note_ret = new NOTICE();
+                        note_ret.ParseTags(split_raw[0]);
+                        output = note_ret;
+                        break;
 
 
-                case Messages.Type.HOSTTARGET:
-                    // TODO: HOSTTARGET Class
-                    output = new Message();
-                    output.Type = Messages.Type.RECONNECT;
-                    break;
+                    case Messages.Type.HOSTTARGET:
+                        // TODO: HOSTTARGET Class
+                        output = new Message();
+                        output.Type = Messages.Type.RECONNECT;
+                        break;
 
 
-                case Messages.Type.CLEARCHAT:
-                    CLEARCHAT clr_ret = new CLEARCHAT();
-                    clr_ret.ParseTags(split_raw[0]);
-                    output = clr_ret;
-                    break;
-                case Messages.Type.USERSTATE:
-                    USERSTATE usr_ret = new USERSTATE();
-                    usr_ret.ParseTags(split_raw[0]);
-                    output = usr_ret;
-                    break;
+                    case Messages.Type.CLEARCHAT:
+                        CLEARCHAT clr_ret = new CLEARCHAT();
+                        clr_ret.ParseTags(split_raw[0]);
+                        output = clr_ret;
+                        break;
+                    case Messages.Type.USERSTATE:
+                        USERSTATE usr_ret = new USERSTATE();
+                        usr_ret.ParseTags(split_raw[0]);
+                        output = usr_ret;
+                        break;
 
 
-                case Messages.Type.RECONNECT:
-                    // TODO: Reconnect Code
-                    output = new Message();
-                    output.Type = type;
-                    break;
+                    case Messages.Type.RECONNECT:
+                        // TODO: Reconnect Code
+                        output = new Message();
+                        output.Type = type;
+                        break;
 
 
-                case Messages.Type.USERNOTICE:
-                    USERNOTICE usrn_ret = new USERNOTICE();
-                    usrn_ret.ParseTags(split_raw[0]);
-                    output = usrn_ret;
-                    break;
-                case Messages.Type.ROOMSTATE:
-                    ROOMSTATE rs_ret = new ROOMSTATE();
-                    rs_ret.ParseTags(split_raw[0]);
-                    output = rs_ret;
-                    break;
-                default:
-                    output = new Message();
-                    output.Type = type;
-                    break;
+                    case Messages.Type.USERNOTICE:
+                        USERNOTICE usrn_ret = new USERNOTICE();
+                        usrn_ret.ParseTags(split_raw[0]);
+                        output = usrn_ret;
+                        break;
+                    case Messages.Type.ROOMSTATE:
+                        ROOMSTATE rs_ret = new ROOMSTATE();
+                        rs_ret.ParseTags(split_raw[0]);
+                        output = rs_ret;
+                        break;
+                    default:
+                        output = new Message();
+                        output.Type = type;
+                        break;
+                }
+                output.User = split_raw[split_raw.Length - 3];
+            }
+            else
+            {
+                output = new Message();
+                output.User = split_raw[0].Remove(0, 1).Split('!')[0];
             }
 
+            output.Type = type;
             output.Raw = raw;
-            output.User = split_raw[split_raw.Length - 3];
-            output.Value = split_raw[split_raw.Length - 1];
+            output.Value = split_raw[split_raw.Length - 1].Remove(0, 1);
             return output;
 
         }
@@ -174,6 +208,7 @@ namespace TwitchCom
             outputStream.WriteLine(message);
             outputStream.Flush();
         }
+
 
     }
 }
